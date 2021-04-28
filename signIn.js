10a27ae6cdebc7ec3fa8c706f4cd6d82a315ac3b
Bhavya kalra <kalrabhavya13@gmail.com>
2021-04-28 15:41:03 +0530

    new update

--- signIn.js
+++ signIn.js
@@ -2,7 +2,7 @@ function redirect(){
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
     if (username == "MBVADevs" & password == "123456") {
-        window.location.replace('./admin.html');
+        window.location.replace('./index.html');
     }
 }
 
